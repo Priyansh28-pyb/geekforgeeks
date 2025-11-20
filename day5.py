bc7cac89e5280a8ba06339c7531916bddd48a3ea
Priyanshu Singh <livepriyansh@gmail.com>
2025-11-20 23:40:45 +0530

    Update day5.py

---
+++ day5.py
@@ -0,0 +1,7 @@
+# Missing Number
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        expected = n * (n + 1) // 2
+        actual = sum(nums)
+        return expected - actual
